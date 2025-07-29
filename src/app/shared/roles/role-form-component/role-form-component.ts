b28a1ad3bb4cefb227c1b89c1a43bd79811fd06d
rust-master <muhammadzaryabrafique@gmail.com>
2025-07-29 13:39:23 +0500

    Add OnDestroy

--- src/app/shared/roles/role-form-component/role-form-component.ts
+++ src/app/shared/roles/role-form-component/role-form-component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -6,6 +6,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDialogRef, MatDialogModule } from '@angular/material/dialog';
 import { RoleService } from '../../services/role.service';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-role-form',
@@ -13,8 +14,10 @@ import { RoleService } from '../../services/role.service';
   templateUrl: './role-form-component.html',
   styleUrl: './role-form-component.scss'
 })
-export class RoleForm {
+export class RoleForm implements OnDestroy {
   newRole: string = '';
+  private destroy$ = new Subject<void>();
+
 
   constructor(
     private roleService: RoleService,
@@ -24,18 +27,26 @@ export class RoleForm {
   addRole() {
     const role = this.newRole.trim();
     if (!role) return;
-    this.roleService.addRole(role).subscribe({
-      next: () => {
-        this.newRole = '';
-        this.closeDialog({ added: true });
-      },
-      error: (err) => {
-        console.error('Failed to add role', err);
-      }
-    });
+
+    this.roleService.addRole(role)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: () => {
+          this.newRole = '';
+          this.closeDialog({ added: true });
+        },
+        error: (err) => {
+          console.error('Failed to add role', err);
+        }
+      });
   }
 
   closeDialog(data?: any) {
     this.dialogRef.close(data);
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
